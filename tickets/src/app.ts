15e25437d5597f6cab47455addb2452c18d2e371
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-03 18:17:03 +0100

    chore: update app ts for tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@rallycoding/common';
+import { errorHandler, NotFound } from '@m0ticketing/common';
 
 const app = express();
 app.set('trust proxy', true);
@@ -15,7 +15,7 @@ app.use(
 );
 
 app.all('*', async (req, res) => {
-  throw new NotFoundError();
+  throw new NotFound();
 });
 
 app.use(errorHandler);
