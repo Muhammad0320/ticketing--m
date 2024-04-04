71268fd2d684e17bcd5e6ce2fdb3c08cecd79cab
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-04 09:28:00 +0100

    chore: update ingress yaml

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -1,33 +1,43 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
-import { errorHandler, NotFoundError } from '@rallycoding/common';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
+import express from "express";
+import "express-async-errors";
+import cookieSession from "cookie-session";
+import { signupRouter } from "./routes/signup";
+import { signinRouter } from "./routes/signin";
+import { signoutRouter } from "./routes/signout";
+import { currentUserRouter } from "./routes/currentUser";
+import { NotFound, errorHandler } from "@m0ticketing/common";
 
 const app = express();
-app.set('trust proxy', true);
-app.use(json());
+
+app.set("trust proxy", true);
+
+app.use(express.json());
+
 app.use(
   cookieSession({
+    httpOnly: true,
     signed: false,
-    secure: process.env.NODE_ENV !== 'test'
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
+console.log("Hi mom");
 
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
+const rootUserUrl = "/api/users";
+
+app.use(rootUserUrl, signupRouter);
+app.use(rootUserUrl, signinRouter);
+app.use(rootUserUrl, signoutRouter);
+
+app.use(rootUserUrl, currentUserRouter);
+
+app.all("*", (req, res) => {
+  throw new NotFound();
 });
 
 app.use(errorHandler);
 
 export { app };
+
+// dism /online /Enable-Feature /FeatureName:TelnetClient
+// muhammawwal@005
