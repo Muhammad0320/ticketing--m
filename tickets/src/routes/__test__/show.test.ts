2f8ea16d6eae8603ee785f1c219e808f710705ca
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 13:26:28 +0100

    chore:  add test that return a 404 when user finds to get a ticket that does not exist

--- tickets/src/routes/__test__/show.test.ts
+++ tickets/src/routes/__test__/show.test.ts
@@ -1,3 +1,14 @@
-it("return a 404 when user finds to get a ticket that does not exist", async () => {});
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+
+it("return a 404 when user finds to get a ticket that does not exist", async () => {
+  const ticketId = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .get("/api/tickets/" + ticketId)
+    .send()
+    .expect(404);
+});
 
 it("return a ticket on valid id", async () => {});
