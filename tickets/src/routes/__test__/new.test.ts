72172c061ffaee346106f3907529f9f1bcf5e810
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 04:45:53 +0100

    chore: add all test its

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,1 +1,9 @@
 it("has a route handler listening to /api/tickets for post request", async () => {});
+
+it("can only we accessed if user is authenticated", async () => {});
+
+it("return an error if user inputs an invalid title", async () => {});
+
+it("return an error when user inputs an invlid price", async () => {});
+
+it("creates a ticket with valid inputs", async () => {});
