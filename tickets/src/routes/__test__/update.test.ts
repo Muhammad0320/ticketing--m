04ef7cc52d5c658eb68a8873d80390fdb06d2a2e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 18:10:05 +0100

    chore: update test ts

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -1,4 +1,13 @@
-it("returns a 401 if the user is not authenticated", async () => {});
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+it("returns a 401 if the user is not authenticated", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put("/api/tickets/" + id)
+    .send({ ticket: "tafseer", price: 50 });
+});
 
 it("returns a 400 if there is no ticket eith such id", async () => {});
 it("returns a 400 if user if user provides invalid price or title", async () => {});
