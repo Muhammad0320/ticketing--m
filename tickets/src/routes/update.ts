4149c33fa874638a318712e408aa80167a0b238c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 18:42:42 +0100

    chore: add input validators

--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,17 +1,32 @@
-import { NotFound, requireAuth } from "@m0ticketing/common";
+import { NotAuthorized, NotFound, requireAuth } from "@m0ticketing/common";
 import express, { Request, Response } from "express";
 import Tickets from "../model/tickets";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.put("/:id", requireAuth, async (req: Request, res: Response) => {
-  const ticket = await Tickets.findById(req.params.id);
+router.put(
+  "/:id",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage("This field is provided"),
+    body("  price ")
+      .isFloat({ gt: 0 })
+      .withMessage("Please provide a valid price"),
+  ],
+  async (req: Request, res: Response) => {
+    const ticket = await Tickets.findById(req.params.id);
 
-  if (!ticket) {
-    throw new NotFound();
+    if (!ticket) {
+      throw new NotFound();
+    }
+
+    if (req.currentUser.id !== ticket.userId) {
+      throw new NotAuthorized();
+    }
+
+    res.status(200).json({ status: "success", ticket });
   }
-
-  res.sendStatus(200);
-});
+);
 
 export { router as updateTicketRouter };
