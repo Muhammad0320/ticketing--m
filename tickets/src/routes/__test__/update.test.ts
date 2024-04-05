46396a3b37adef1f326f6069db840f75430ece89
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 18:13:30 +0100

    chore: add test to returns a 400 if there is no ticket with such id

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -6,10 +6,19 @@ it("returns a 401 if the user is not authenticated", async () => {
 
   await request(app)
     .put("/api/tickets/" + id)
-    .send({ ticket: "tafseer", price: 50 });
+    .send({ ticket: "tafseer", price: 50 })
+    .expect(401);
 });
 
-it("returns a 400 if there is no ticket eith such id", async () => {});
+it("returns a 400 if there is no ticket eith such id", async () => {
+  const id = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app)
+    .put("/api/tickets/" + id)
+    .set("Cookie", global.signin())
+    .send({ ticket: "tafseer", price: 50 })
+    .expect(400);
+});
 it("returns a 400 if user if user provides invalid price or title", async () => {});
 it("returns a 401 if user tries to update other peoples ticket", async () => {});
 it("updates ticket with valid inputs", async () => {});
