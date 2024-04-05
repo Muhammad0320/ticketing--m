2c17bc0d5b961943e1462876e7a4e4a489f4683d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 18:07:01 +0100

    chore: update test ts

---
+++ tickets/src/routes/__test__/update.test.ts
@@ -0,0 +1,6 @@
+it("returns a 401 if the user is not authenticated", async () => {});
+
+it("returns a 400 if there is no ticket eith such id", async () => {});
+it("returns a 400 if user if user provides invalid price or title", async () => {});
+it("returns a 401 if user tries to update other peoples ticket", async () => {});
+it("updates ticket with valid inputs", async () => {});
