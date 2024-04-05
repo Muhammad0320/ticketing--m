c38f1e0a887122a4e1eeadad1e3e770a1abb6f1e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 09:51:13 +0100

    chore: update new test ts logic

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,11 +1,25 @@
 import express, { Request, Response } from "express";
 
-import { requireAuth } from "@m0ticketing/common";
+import { requestValidator, requireAuth } from "@m0ticketing/common";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/", requireAuth, async (req: Request, res: Response) => {
-  res.sendStatus(201);
-});
+router.post(
+  "/",
+  requireAuth,
+  [
+    body("title").not().isEmpty().withMessage(" Title is required "),
+    body("price")
+      .isFloat({ gt: 0 })
+      .withMessage(" Price should be greater than 0 "),
+  ],
+
+  requestValidator,
+
+  async (req: Request, res: Response) => {
+    res.sendStatus(201);
+  }
+);
 
 export { router as createTicketRouter };
