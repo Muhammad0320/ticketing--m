740f3ee5b9653d4cc6bac75ad377923c9f8000b0
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 17:45:43 +0100

    chore: update send index test

--- tickets/src/routes/__test__/index.test.ts
+++ tickets/src/routes/__test__/index.test.ts
@@ -1,1 +1,19 @@
-it("fetcches all tickets", async () => {});
+import request from "supertest";
+import { app } from "../../app";
+
+const fetchTicket = () => {
+  return request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({ title: "Tafseer", price: 10 })
+    .expect(201);
+};
+
+it("fetches all tickets", async () => {
+  await fetchTicket();
+  await fetchTicket();
+  await fetchTicket();
+  await fetchTicket();
+
+  const response = await request(app).get("/api/tickets").send().expect(200);
+});
