c86385a57c0c67f85c188f1514d5df305a3e7e95
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 17:50:23 +0100

    chore: update index ts

--- tickets/src/routes/index.ts
+++ tickets/src/routes/index.ts
@@ -1,5 +1,16 @@
 import express from "express";
+import Tickets from "../model/tickets";
+import { Request, Response } from "express";
 
 const router = express.Router();
+
+router.get("/", async (req: Request, res: Response) => {
+  const ticket = await Tickets.find({});
+
+  res.status(200).json({
+    status: "success",
+    data: ticket,
+  });
+});
 
 export { router as ticketIndexRouter };
