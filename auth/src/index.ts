1054f47794765ab8bc1112fa24aef9c217c9d241
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 04:31:31 +0100

    chore: updae the auth index

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,31 +1,34 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-import { app } from './app';
+import { app } from "./app";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
-    throw new Error('JWT_KEY must be defined');
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
   }
 
   // my-super-long-and-ultra-secure-jwt-secret
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URI);
 
-    console.log('Connected to MongoDb');
+    console.log("Connected to MongoDb");
   } catch (err) {
     console.error(err);
   }
 
   app.listen(3000, () => {
-    console.log('Listening on port 3000!!!!!!!!');
+    console.log("Listening on port 3000!!!!!!!!");
   });
 };
 
 start();
 
-
-  /*
+/*
     34.134.113.44 ticketing.dev
 34.68.142.10 ticketing--m.dev 
     */
