2ddb47cb6deb3f2f429e00402bcc59f25f655194
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 05:02:34 +0100

    chore: wired up new router with app

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,20 +1,25 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
-import { errorHandler, NotFound } from '@m0ticketing/common';
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFound } from "@m0ticketing/common";
+import { createTicketRouter } from "./routes/__test__/new";
 
 const app = express();
-app.set('trust proxy', true);
+app.set("trust proxy", true);
 app.use(json());
 app.use(
   cookieSession({
     signed: false,
-    secure: process.env.NODE_ENV !== 'test',
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 
-app.all('*', async (req, res) => {
+const rootUrl = "/api/tickets";
+
+app.use(rootUrl, createTicketRouter);
+
+app.all("*", async (req, res) => {
   throw new NotFound();
 });
 
