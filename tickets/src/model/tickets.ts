9e85d52f9bb7b149ef1f06a0477703136ae906ae
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 10:19:41 +0100

    chore: update ticket schema

--- tickets/src/model/tickets.ts
+++ tickets/src/model/tickets.ts
@@ -11,3 +11,22 @@ type TicketDoc = mongoose.Document & TicketAttrs;
 interface TicketModel extends mongoose.Model<TicketDoc> {
   buildTicket(attrs: TicketAttrs): TicketDoc;
 }
+
+const ticketSchema = new mongoose.Schema({
+  userId: {
+    type: String,
+
+    require: [true, "This field is required"],
+  },
+
+  title: {
+    type: String,
+    require: [true, "This field is required"],
+  },
+
+  price: {
+    type: Number,
+
+    require: [true, "This field is required"],
+  },
+});
