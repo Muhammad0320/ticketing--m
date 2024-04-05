eb7ae53ccd01348e53b8ae4fc7ed04201831e20d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-05 05:01:05 +0100

    chore: update new test ts

--- tickets/src/routes/__test__/new.ts
+++ tickets/src/routes/__test__/new.ts
@@ -1,3 +1,9 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const router = express.Router();
+
+router.post("/", async (req: Request, res: Response) => {
+  res.sendStatus(201);
+});
+
+export { router as createTicketRouter };
