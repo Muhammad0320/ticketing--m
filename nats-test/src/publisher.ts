ad5913c86bcf1de76ed98aa7a76d9b009fa9a5f7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-06 11:07:10 +0100

    chore:  update nats publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,9 @@
-import nats from "node-nats-streaming";
+import nats, { Stan } from "node-nats-streaming";
 
 const stan = nats.connect("ticketing", "shitt", {
   url: "http://localhost:4222",
+});
+
+stan.on("connect", () => {
+  console.log("publisher connected to NATS!");
 });
