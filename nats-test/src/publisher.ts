cae78714fef68ac4e866e78e0c96ef834cfe1427
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-06 17:25:26 +0100

    chore: update publisher

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,6 +1,6 @@
 import nats, { Stan } from "node-nats-streaming";
 
-const stan = nats.connect("ticketing", "shitt", {
+const stan = nats.connect("ticketing--m", "shitt", {
   url: "http://localhost:4222",
   connectTimeout: 100 * 1000,
 });
