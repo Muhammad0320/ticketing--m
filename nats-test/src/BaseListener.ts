a68cfdbfd8efe776b9a362367d410bc1f3c63139
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-07 18:32:55 +0100

    chore: add base listener class

---
+++ nats-test/src/BaseListener.ts
@@ -0,0 +1,52 @@
+import { Message, Stan, SubscriptionOptions } from "node-nats-streaming";
+
+export abstract class Listener {
+  abstract subject: string;
+
+  abstract queueGroupName: string;
+
+  abstract onMessage(data: any, msg: Message): void;
+
+  private client: Stan;
+
+  protected ackWait: number = 5 * 1000;
+
+  constructor(client: Stan) {
+    this.client = client;
+  }
+
+  subscriptionOptions(): SubscriptionOptions {
+    return this.client
+      .subscriptionOptions()
+      .setAckWait(this.ackWait)
+      .setManualAckMode(true)
+      .setDeliverAllAvailable()
+      .setDurableName(this.queueGroupName);
+  }
+
+  listen() {
+    const subscription = this.client.subscribe(
+      this.subject,
+      this.queueGroupName,
+      this.subscriptionOptions()
+    );
+
+    subscription.on("message", async (msg: Message) => {
+      console.log(
+        ` Received event ${msg.getSequence()} ${this.subject} / ${
+          this.queueGroupName
+        } `
+      );
+
+      const data = this.parseMessage(msg);
+
+      this.onMessage(data, msg);
+    });
+  }
+
+  parseMessage(msg: Message) {
+    const data = msg.getData() as string;
+
+    return JSON.parse(data);
+  }
+}
