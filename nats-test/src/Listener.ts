5f085f43a9aa84a5d6d8c64223128109c510fe1d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-07 07:43:12 +0100

    chore: add queue group name

--- nats-test/src/Listener.ts
+++ nats-test/src/Listener.ts
@@ -1,15 +1,19 @@
+import { randomBytes } from "crypto";
 import nats, { Message } from "node-nats-streaming";
 
 console.clear();
 
-const stan = nats.connect("ticketing--m", "223", {
+const stan = nats.connect("ticketing--m", randomBytes(4).toString("hex"), {
   connectTimeout: 100 * 1000,
 });
 
 stan.on("connect", () => {
   console.log("Event Listener created");
 
-  const subscription = stan.subscribe("ticket:created");
+  const subscription = stan.subscribe(
+    "ticket:created",
+    "order-service-queue-group"
+  );
 
   subscription.on("message", (msg: Message) => {
     const data = msg.getData() as string;
