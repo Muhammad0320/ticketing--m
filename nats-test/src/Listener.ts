9d9b3bf70ad4db7e74075ddc09a8af8494321718
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-07 07:17:39 +0100

    chore: update listener an dpublisher

--- nats-test/src/Listener.ts
+++ nats-test/src/Listener.ts
@@ -1,5 +1,7 @@
 import nats from "node-nats-streaming";
 
+console.clear();
+
 const stan = nats.connect("ticketing--m", "223", {
   connectTimeout: 100 * 1000,
 });
--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,7 @@
 import nats from "node-nats-streaming";
 
+console.clear();
+
 const stan = nats.connect("ticketing--m", "shitt", {
   url: "http://localhost:4222",
   connectTimeout: 100 * 1000,
