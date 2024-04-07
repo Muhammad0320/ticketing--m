afcd5054a5308c8b7ee48920f6025da66da2e2da
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-07 20:02:26 +0100

    chore: used ts to help ourselves

--- nats-test/src/events/TicketCrearedListener.ts
+++ nats-test/src/events/TicketCrearedListener.ts
@@ -1,12 +1,14 @@
 import { Message } from "node-nats-streaming";
 import { Listener } from "./BaseListener";
+import { TicketCreatedEvent } from "./TicketCreatedEvent";
+import { Subjects } from "./Subjects";
 
-export class TicketCreatedListenr extends Listener {
-  subject = "ticket:created";
+export class TicketCreatedListenr extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
 
   queueGroupName = "payment-service";
 
-  onMessage(data: any, msg: Message) {
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
     console.log(`Event data!`, data);
 
     msg.ack();
