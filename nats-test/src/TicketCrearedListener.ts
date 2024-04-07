d34aaba5a197fa62953f1ffe0f1732c3a5031349
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-07 18:34:25 +0100

    chore: add ticket creared listenner

---
+++ nats-test/src/TicketCrearedListener.ts
@@ -0,0 +1,14 @@
+import { Message } from "node-nats-streaming";
+import { Listener } from "./BaseListener";
+
+export class TicketCreatedListenr extends Listener {
+  subject = "ticket:created";
+
+  queueGroupName = "payment-service";
+
+  onMessage(data: any, msg: Message) {
+    console.log(`Event data!: ${JSON.stringify(data)}`);
+
+    msg.ack();
+  }
+}
