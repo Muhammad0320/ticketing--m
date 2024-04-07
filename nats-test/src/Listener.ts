1038ad9e7e2c674b9268e3c839ccca9214320064
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-07 06:53:32 +0100

    chore: update listenenr

--- nats-test/src/Listener.ts
+++ nats-test/src/Listener.ts
@@ -3,3 +3,13 @@ import nats from "node-nats-streaming";
 const stan = nats.connect("ticketing--m", "223", {
   connectTimeout: 100 * 1000,
 });
+
+stan.on("connect", () => {
+  console.log("Event Listener created");
+
+  const subscription = stan.subscribe("ticket:created");
+
+  subscription.on("message", () => {
+    console.log("Message received");
+  });
+});
