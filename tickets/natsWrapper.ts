425a17db595d51655d494c196161dcff86351284
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-08 11:12:20 +0100

    chore: update natsWrapper

--- tickets/natsWrapper.ts
+++ tickets/natsWrapper.ts
@@ -1,1 +1,24 @@
-class NatsWrapper {}
+import { Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
+
+class NatsWrapper {
+  private _client?: Stan;
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this._client!.on("connect", () => {
+        console.log("Connected to NATS");
+
+        resolve();
+      });
+
+      this._client!.on("error", (err: any) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
