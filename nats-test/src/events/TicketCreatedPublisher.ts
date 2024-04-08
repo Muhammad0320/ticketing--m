0c72d47504b01631db055ef096f449731889108e
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-08 04:57:51 +0100

    chore: conpmplete ticket created oublisher

--- nats-test/src/events/TicketCreatedPublisher.ts
+++ nats-test/src/events/TicketCreatedPublisher.ts
@@ -1,1 +1,7 @@
-export class TicketCreatedPublisher {}
+import { Publisher } from "./BasePublisher";
+import { Subjects } from "./Subjects";
+import { TicketCreatedEvent } from "./TicketCreatedEvent";
+
+export class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
+  readonly subjects = Subjects.TicketCreated;
+}
