f5f96385ebcfa8f8ce21acf018bba2d30e651856
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-09 12:15:48 +0100

    chore:  add files to orders src

---
+++ orders/src/app.ts
@@ -0,0 +1,36 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { currentUser, errorHandler, NotFound } from "@m0ticketing/common";
+import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
+import { ticketIndexRouter } from "./routes";
+import { updateTicketRouter } from "./routes/update";
+
+const app = express();
+app.set("trust proxy", true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+
+app.use(currentUser);
+
+const rootUrl = "/api/tickets";
+
+app.use(rootUrl, createTicketRouter);
+app.use(rootUrl, showTicketRouter);
+app.use(rootUrl, ticketIndexRouter);
+app.use(rootUrl, updateTicketRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFound();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ orders/src/index.ts
@@ -0,0 +1,44 @@
+import { app } from "./app";
+import mongoose from "mongoose";
+import { randomBytes } from "crypto";
+import { natsWrapper } from "./natsWrapper";
+
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error("JWT_KEY must be defined");
+  }
+
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
+
+  try {
+    await natsWrapper.connect(
+      "ticketing--m",
+      randomBytes(4).toString("hex"),
+      "http://nats-srv:4222"
+    );
+
+    natsWrapper.client.on("close", () => {
+      console.log(" NATS connection closed! ");
+      console.log("Who's behind this");
+      process.exit();
+    });
+
+    process.on("SIGTERM", () => natsWrapper.client.close());
+    process.on("SIGINT", () => natsWrapper.client.close());
+
+    await mongoose.connect(process.env.MONGO_URI);
+
+    console.log("Connected to MongoDb");
+  } catch (err) {
+    console.log("Let's see");
+    console.error(err);
+  }
+
+  app.listen(3000, () => {
+    console.log("Listening on port 3000!!!!!!!!");
+  });
+};
+
+start();
