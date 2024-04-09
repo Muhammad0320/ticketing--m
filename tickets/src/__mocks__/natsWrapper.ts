08dedee27a3fb45ff5dc028d4a751d6ef22760c9
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-09 05:36:27 +0100

    chore: update mock file

--- tickets/src/__mocks__/natsWrapper.ts
+++ tickets/src/__mocks__/natsWrapper.ts
@@ -1,5 +1,7 @@
-export const client = {
-  publish(subject: string, data: string, callback: () => Promise<void>) {
-    callback();
+export const natsWrapper = {
+  client: {
+    publish(subject: string, data: string, callback: () => Promise<void>) {
+      callback();
+    },
   },
 };
