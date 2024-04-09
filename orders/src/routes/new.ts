a88d92d310dd0cf01b392f3733861ca38454f1c7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-09 13:16:00 +0100

    chore: add body validator

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,7 +1,21 @@
+import { requireAuth } from "@m0ticketing/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
-router.post("/", async (req: Request, res: Response) => {});
+router.post(
+  "/",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Please provide a valid ticketId"),
+  ],
+  async (req: Request, res: Response) => {}
+);
 
 export { router as newOrderRouter };
