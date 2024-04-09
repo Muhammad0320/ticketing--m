c47e51818f96ad5f35dbbaafdb2ff745b1752273
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-09 17:18:56 +0100

    chore: update build

--- common/build/events/TicketCreatedPublisher.d.ts
+++ common/build/events/TicketCreatedPublisher.d.ts
@@ -1,5 +1,5 @@
-import { Publisher } from "./BasePublisher";
 import { Subjects } from "./Subjects";
+import { Publisher } from "./BasePublisher";
 import { TicketCreatedEvent } from "./TicketCreatedEvent";
 export declare class TicketCreatedPublisher extends Publisher<TicketCreatedEvent> {
     readonly subject = Subjects.TicketCreated;
--- common/build/events/TicketCreatedPublisher.js
+++ common/build/events/TicketCreatedPublisher.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.TicketCreatedPublisher = void 0;
-const BasePublisher_1 = require("./BasePublisher");
 const Subjects_1 = require("./Subjects");
+const BasePublisher_1 = require("./BasePublisher");
 class TicketCreatedPublisher extends BasePublisher_1.Publisher {
     constructor() {
         super(...arguments);
