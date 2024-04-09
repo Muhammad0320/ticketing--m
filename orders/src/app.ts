cdff66b9164cd7dfbc32b4d0911051876b1f4b9d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-09 13:11:32 +0100

    chore: update app ts

--- orders/src/app.ts
+++ orders/src/app.ts
@@ -3,6 +3,10 @@ import "express-async-errors";
 import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { currentUser, errorHandler, NotFound } from "@m0ticketing/common";
+import { indexOrderRouter } from "./routes";
+import { deleteOrderRouter } from "./routes/delete";
+import { showOrderRouter } from "./routes/show";
+import { newOrderRouter } from "./routes/new";
 
 const app = express();
 app.set("trust proxy", true);
@@ -17,6 +21,11 @@ app.use(
 app.use(currentUser);
 
 const rootUrl = "/api/orders";
+
+app.use(rootUrl, indexOrderRouter);
+app.use(rootUrl, deleteOrderRouter);
+app.use(rootUrl, showOrderRouter);
+app.use(rootUrl, newOrderRouter);
 
 app.all("*", async (req, res) => {
   throw new NotFound();
