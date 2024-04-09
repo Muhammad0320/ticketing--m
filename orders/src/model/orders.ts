d8f5d107ed2e5b749340a4f691a5abe1d8ec61e4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-09 17:31:08 +0100

    chore: add order schema

--- orders/src/model/orders.ts
+++ orders/src/model/orders.ts
@@ -13,3 +13,34 @@ type OrderDoc = mongoose.Document & OrderAttrs;
 interface OrderModel extends mongoose.Model<OrderDoc> {
   buildOrder(attrs: OrderAttrs): OrderDoc;
 }
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Types.ObjectId,
+      required: [true, "An order must belong to a user"],
+    },
+
+    status: {
+      type: String,
+      enum: Object.values(OrderStatus),
+      default: OrderStatus.Created,
+    },
+
+    expiresAt: Date,
+
+    ticket: {
+      type: mongoose.Types.ObjectId,
+      ref: "Ticket",
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+
+        delete ret._id;
+      },
+    },
+  }
+);
