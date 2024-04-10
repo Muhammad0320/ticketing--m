bf44926e03f96d62288c09928df3372511f39674
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 20:05:26 +0100

    chore: write test that publishes an order cancelled event

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,6 +1,7 @@
 import { app } from "../../app";
 import Ticket from "../../model/tickets";
 import request from "supertest";
+import { natsWrapper } from "../../natsWrapper";
 
 it("cancels an order", async () => {
   const ticket = await Ticket.buildTicket({ title: "shit", price: 90 });
@@ -20,4 +21,26 @@ it("cancels an order", async () => {
     .set("Cookie", user)
     .send()
     .expect(204);
+});
+
+it("publishes an order cancelled event", async () => {
+  const ticket = await Ticket.buildTicket({ title: "shit", price: 90 });
+
+  const user = global.signin();
+
+  const {
+    body: { data: orderData },
+  } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${orderData.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
 });
