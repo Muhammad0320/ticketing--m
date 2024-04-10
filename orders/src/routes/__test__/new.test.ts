037c08a8e74692d3db1814156414bedea2a2a683
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 04:51:00 +0100

    chore: update test file

---
+++ orders/src/routes/__test__/new.test.ts
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import request from "supertest";
+import { app } from "../../app";
+
+it("returns a 404 if ticket does not exist", async () => {
+  const ticketId = new mongoose.Types.ObjectId().toHexString();
+
+  await request(app).post("/api/orders").set("Cookie", "");
+});
+
+it("returns a 400 if a ticket is already reserved", async () => {});
+
+it("creates a ticket", async () => {});
