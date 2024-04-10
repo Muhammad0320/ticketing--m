b773989d41341e0959842f91609ffc10d8d38525
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 15:47:57 +0100

    chore:  add test that cancels an order

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -1,1 +1,23 @@
-it("cancels an order", async () => {});
+import { app } from "../../app";
+import Ticket from "../../model/tickets";
+import request from "supertest";
+
+it("cancels an order", async () => {
+  const ticket = await Ticket.buildTicket({ title: "shit", price: 90 });
+
+  const user = global.signin();
+
+  const {
+    body: { data: orderData },
+  } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${orderData.id}`)
+    .set("Cookie", user)
+    .send()
+    .expect(204);
+});
