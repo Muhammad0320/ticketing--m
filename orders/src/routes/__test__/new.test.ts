060553a57250c926e0a63d8a385e79e87b504073
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 04:56:53 +0100

    chore: add test that returns a 404 if ticket does not exist

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -5,7 +5,11 @@ import { app } from "../../app";
 it("returns a 404 if ticket does not exist", async () => {
   const ticketId = new mongoose.Types.ObjectId().toHexString();
 
-  await request(app).post("/api/orders").set("Cookie", "");
+  await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId })
+    .expect(404);
 });
 
 it("returns a 400 if a ticket is already reserved", async () => {});
