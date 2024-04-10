b5e32459e7a6f00676134eaada2b7403d09d742a
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 14:20:02 +0100

    chore: update show.ts

--- orders/src/routes/show.ts
+++ orders/src/routes/show.ts
@@ -1,7 +1,17 @@
 import express, { Request, Response } from "express";
+import Orders from "../model/orders";
+import { NotFound } from "@m0ticketing/common";
 
 const router = express.Router();
 
-router.get("/:orderId", async (req: Request, res: Response) => {});
+router.get("/:orderId", async (req: Request, res: Response) => {
+  const { orderId } = req.params;
+
+  const order = await Orders.findById(orderId);
+
+  if (!order) {
+    throw new NotFound();
+  }
+});
 
 export { router as showOrderRouter };
