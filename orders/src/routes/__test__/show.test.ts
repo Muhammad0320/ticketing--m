d669d69aba18d02a3230e40ca2a078a5ae85efb4
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 14:47:05 +0100

    chore: add test that return a 400 if user tried to find other peoples order

--- orders/src/routes/__test__/show.test.ts
+++ orders/src/routes/__test__/show.test.ts
@@ -1,6 +1,7 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
+import Ticket from "../../model/tickets";
 
 it("returns a 404 if the order does not exist", async () => {
   const orderId = new mongoose.Types.ObjectId().toHexString();
@@ -10,4 +11,22 @@ it("returns a 404 if the order does not exist", async () => {
     .set("Cookie", global.signin())
     .send({})
     .expect(404);
+});
+
+it("return a 400 if user tried to find other people's  order ", async () => {
+  const ticket = await Ticket.buildTicket({ price: 99, title: "shit ticket" });
+
+  const {
+    body: { data: orderData },
+  } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", global.signin())
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .get(`/api/orders/${orderData.id}`)
+    .set("Cookie", global.signin())
+    .send()
+    .expect(400);
 });
