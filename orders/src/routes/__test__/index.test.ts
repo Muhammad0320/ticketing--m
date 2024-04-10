62a766bd194d37c7bb6462b9086e7bc7d6bcc604
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 05:55:52 +0100

    chore: created 3 tickets and order

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,4 +1,6 @@
+import { app } from "../../app";
 import Ticket from "../../model/tickets";
+import request from "supertest";
 
 const buildTicket = async () => {
   const ticket = await Ticket.buildTicket({ price: 99, title: "sport event" });
@@ -6,4 +8,34 @@ const buildTicket = async () => {
   return ticket;
 };
 
-it("fetckes all the tickets that belogs to a user", async () => {});
+it("fetckes all the tickets that belogs to a user", async () => {
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  const user1 = global.signin();
+
+  const user2 = global.signin();
+
+  const {
+    body: { data: order1 },
+  } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user1)
+    .send({ ticketId: ticketOne.id })
+    .expect(201);
+  const {
+    body: { data: order2 },
+  } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user2)
+    .send({ ticketId: ticketTwo.id })
+    .expect(201);
+  const {
+    body: { data: order3 },
+  } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", user2)
+    .send({ ticket: ticketThree.id })
+    .expect(201);
+});
