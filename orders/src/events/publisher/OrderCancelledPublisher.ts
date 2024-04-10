7cba4375a1f5c3f010a49f0fbbf87aba2da26ec3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 18:51:27 +0100

    chore: add order created publisher

---
+++ orders/src/events/publisher/OrderCancelledPublisher.ts
@@ -0,0 +1,5 @@
+import { OrderCancelledEvent, Publisher, Subjects } from "@m0ticketing/common";
+
+export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+}
