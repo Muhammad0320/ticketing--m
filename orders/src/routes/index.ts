4d8e26390d0c1adb02365b627e30834bbbd6fe5f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 05:47:24 +0100

    chore: update index ts

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -1,7 +1,13 @@
+import { requireAuth } from "@m0ticketing/common";
 import express, { Request, Response } from "express";
+import Orders from "../model/orders";
 
 const router = express.Router();
 
-router.get("/", async (req: Request, res: Response) => {});
+router.get("/", requireAuth, async (req: Request, res: Response) => {
+  const orders = await Orders.find({ userId: req.currentUser?.id });
+
+  res.status(200).json({ status: "success", data: orders });
+});
 
 export { router as indexOrderRouter };
