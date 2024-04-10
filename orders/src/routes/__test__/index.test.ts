cf4249551d6de3317625d3958f2cd119aefad39c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-10 05:50:37 +0100

    chore: add setup function

--- orders/src/routes/__test__/index.test.ts
+++ orders/src/routes/__test__/index.test.ts
@@ -1,1 +1,9 @@
+import Ticket from "../../model/tickets";
+
+const buildTicket = async () => {
+  const ticket = await Ticket.buildTicket({ price: 99, title: "sport event" });
+
+  return ticket;
+};
+
 it("fetckes all the tickets that belogs to a user", async () => {});
