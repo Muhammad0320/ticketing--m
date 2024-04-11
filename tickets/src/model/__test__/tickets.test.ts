df8e82a37c93a6c18609e6d1bd2fafca778be10f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-11 11:22:12 +0100

    chore: update tickets test ts

--- tickets/src/model/__test__/tickets.test.ts
+++ tickets/src/model/__test__/tickets.test.ts
@@ -1,1 +1,30 @@
-it("implements optimistic concurrency control", async () => {});
+import mongoose from "mongoose";
+import Tickets from "../tickets";
+
+it("implements optimistic concurrency control", async () => {
+  // Create a new ticket
+  const ticket = await Tickets.buildTicket({
+    title: "shit",
+    price: 90,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  });
+
+  // Fetch the created ticket w/ 2 intances
+  const ticketInstance1 = await Tickets.findById(ticket.id);
+  const ticketInstance2 = await Tickets.findById(ticket.id);
+
+  // Update both instances
+
+  ticketInstance1?.set({ price: 60 });
+  ticketInstance2?.set({ price: 70 });
+
+  // save the first instace
+
+  await ticketInstance1?.save();
+
+  // then the second
+
+  expect(async () => {
+    await ticketInstance2?.save();
+  }).toThrow();
+});
