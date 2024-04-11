0956a73e375b50a6dcccafaf8940ef8dac40a2f7
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-11 10:42:28 +0100

    chore:  update ticket updated listenner

--- orders/src/events/listenenr/TicketUpdatedListener.ts
+++ orders/src/events/listenenr/TicketUpdatedListener.ts
@@ -1,1 +1,24 @@
-export class TicketUpdatedListener {}
+import { Listener, Subjects, TicketUpdatedEvent } from "@m0ticketing/common";
+import { queueGroupName } from "./queueGroupName";
+import { Message } from "node-nats-streaming";
+import Ticket from "../../model/tickets";
+
+export class TicketUpdatedListener extends Listener<TicketUpdatedEvent> {
+  readonly subject = Subjects.TicketUpdated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: TicketUpdatedEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    const { title, price } = data;
+
+    ticket.set({ title, price });
+    await ticket.save();
+    msg.ack();
+  }
+}
