3e49d6440d278f525816a94c71293ada2a3a224d
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-12 12:25:10 +0100

    chore: update order created listenner

--- tickets/src/events/listener/__test__/OrderCreatedListenner.test.ts
+++ tickets/src/events/listener/__test__/OrderCreatedListenner.test.ts
@@ -1,1 +1,35 @@
+import { OrderCreatedEvent, OrderStatus } from "@m0ticketing/common";
+import Tickets from "../../../model/tickets";
+import { natsWrapper } from "../../../natsWrapper";
+import { OrderCreatedListener } from "../OrderCreatedListener";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+
+const setup = async () => {
+  const listener = new OrderCreatedListener(natsWrapper.client);
+
+  const ticket = await Tickets.buildTicket({
+    userId: "jbebgibgigr",
+    title: "shit title",
+    price: 34,
+  });
+
+  const data: OrderCreatedEvent["data"] = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    status: OrderStatus.Created,
+    userId: "shit user",
+    expiresAt: "shit day",
+    ticket: {
+      id: ticket.id,
+      price: ticket.price,
+    },
+  };
+
+  // @ts-ignore
+
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+};
+
 it("add orderId to ordered ticket", async () => {});
