dd278e7aa4c0b9114dbe98ecc81dabbc2b6a57e3
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-12 13:30:55 +0100

    chore: pulblish an event inside a listenner

--- tickets/src/events/listener/OrderCreatedListener.ts
+++ tickets/src/events/listener/OrderCreatedListener.ts
@@ -2,6 +2,7 @@ import { Listener, OrderCreatedEvent, Subjects } from "@m0ticketing/common";
 import { queueGroupName } from "./queueGroupName";
 import { Message } from "node-nats-streaming";
 import Tickets from "../../model/tickets";
+import { TicketUpdatedPublisher } from "../publisher/TicketUpdatedPublisher";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
@@ -18,6 +19,16 @@ export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
     ticket.set({ orderId: data.id });
 
     await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      userId: ticket.userId,
+
+      version: ticket.version,
+      title: ticket.title,
+      price: ticket.price,
+      orderId: ticket.orderId,
+    });
 
     msg.ack();
   }
