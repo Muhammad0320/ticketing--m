34ac5125f99a134bff25317da541b65cbe006cec
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-12 12:12:24 +0100

    chore: update order created lisnener

--- tickets/src/events/listener/OrderCreatedListener.ts
+++ tickets/src/events/listener/OrderCreatedListener.ts
@@ -1,1 +1,22 @@
-export class OrderCreatedListener {}
+import { Listener, OrderCreatedEvent, Subjects } from "@m0ticketing/common";
+import { queueGroupName } from "./queueGroupName";
+import { Message } from "node-nats-streaming";
+import Tickets from "../../model/tickets";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const ticket = await Tickets.findById(data.ticket.id);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    ticket.set({ orderId: data.id });
+
+    msg.ack();
+  }
+}
