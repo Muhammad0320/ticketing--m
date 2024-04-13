f07bfea777af18b2784b896c9d6512ccb8fd155c
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-13 11:39:10 +0100

    chore: update ordercreated listenner

--- expiration/src/event/listener/OrderCreatedListener.ts
+++ expiration/src/event/listener/OrderCreatedListener.ts
@@ -1,11 +1,18 @@
 import { Listener, OrderCreatedEvent, Subjects } from "@m0ticketing/common";
 import { queueGroupName } from "./queuegroupName";
 import { Message } from "node-nats-streaming";
+import { expirationQueue } from "../../queues/expirationQueue";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   readonly subject = Subjects.OrderCreated;
 
   queueGroupName = queueGroupName;
 
-  onMessage(data: OrderCreatedEvent["data"], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add({ orderId: data.id }, { delay });
+
+    msg.ack();
+  }
 }
