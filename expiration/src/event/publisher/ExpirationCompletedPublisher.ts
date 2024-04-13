edb2429a3a8ad69a3bba013f5af106cd1e47b239
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-13 12:02:01 +0100

    chore: complete the ExpirationCompletePublisher

--- expiration/src/event/publisher/ExpirationCompletedPublisher.ts
+++ expiration/src/event/publisher/ExpirationCompletedPublisher.ts
@@ -1,1 +1,9 @@
-export class ExpirationCompletePublisher {}
+import {
+  ExpirationCompleteEvent,
+  Publisher,
+  Subjects,
+} from "@m0ticketing/common";
+
+export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationCompleted;
+}
