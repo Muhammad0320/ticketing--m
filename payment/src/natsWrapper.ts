48ff732c405353e7478896b1d42c680784da4edc
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-13 19:41:08 +0100

    chore: add src

---
+++ payment/src/natsWrapper.ts
@@ -0,0 +1,35 @@
+import { Stan } from "node-nats-streaming";
+import nats from "node-nats-streaming";
+
+class NatsWrapper {
+  private _client?: Stan;
+
+  get client() {
+    if (!this._client) {
+      console.log("Shoooo");
+      throw new Error(" Client is not initialized before connection ");
+    }
+
+
+    return this._client;
+  }
+
+  connect(clusterId: string, clientId: string, url: string) {
+    this._client = nats.connect(clusterId, clientId, { url });
+
+    return new Promise<void>((resolve, reject) => {
+      this.client.on("connect", () => {
+        console.log("Connected to NATS !");
+        console.log("Gotten 2");
+
+        resolve();
+      });
+
+      this.client.on("error", (err: any) => {
+        reject(err);
+      });
+    });
+  }
+}
+
+export const natsWrapper = new NatsWrapper();
