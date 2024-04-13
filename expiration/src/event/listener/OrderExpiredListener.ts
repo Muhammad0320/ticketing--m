c4eab9a2a996858e81d9f29ed808bf46ff6e7f14
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-13 07:26:53 +0100

    chore:  changed to order creared event

--- expiration/src/event/listener/OrderExpiredListener.ts
+++
@@ -1,7 +0,0 @@
-import { Listener } from "@m0ticketing/common";
-
-interface OrderExpiredListneer {
-  orderId: string;
-}
-
-export class OrderExpiredListener extends Listener<OrderExpiredListneer> {}
