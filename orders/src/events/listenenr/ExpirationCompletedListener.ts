8cb71566e2df0da9585bce92c43128f477a00f27
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-13 12:26:41 +0100

    chore: update expiration completed listenner

--- orders/src/events/listenenr/ExpirationCompletedListener.ts
+++ orders/src/events/listenenr/ExpirationCompletedListener.ts
@@ -1,1 +1,32 @@
-export class ExpirationCompletedListener {}
+import {
+  ExpirationCompleteEvent,
+  Listener,
+  OrderStatus,
+  Subjects,
+} from "@m0ticketing/common";
+import { queueGroupName } from "./queueGroupName";
+import { Message } from "node-nats-streaming";
+import Orders from "../../model/orders";
+import { OrderCancelledPublisher } from "../publisher/OrderCancelledPublisher";
+
+export class ExpirationCompletedListener extends Listener<ExpirationCompleteEvent> {
+  readonly subject = Subjects.ExpirationCompleted;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+    const order = await Orders.findById(data.id).populate("ticket");
+
+    if (!order) {
+      throw new Error("Order not found");
+    }
+
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    await new OrderCancelledPublisher(this.client).publish({
+      id: order.id,
+      ticket: { id: order.ticket.id },
+    });
+  }
+}
