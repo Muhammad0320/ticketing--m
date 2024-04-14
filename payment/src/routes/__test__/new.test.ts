f58c776e392b15cbd36c028ac3c7665294262f72
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-14 17:57:41 +0100

    chore: update new test ts

---
+++ payment/src/routes/__test__/new.test.ts
@@ -0,0 +1,5 @@
+it("returns a 404 if there is no order with such id", async () => {});
+
+it("returns a 401 if users tried to pay for a ticket that they don't own", async () => {});
+
+it("returns a 400, if a calcelled order is orderes", async () => {});
