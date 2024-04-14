a209e807c63015fe9188b4eddd5f39a5097a83ec
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-14 17:45:58 +0100

    chore: update new test ts

--- payment/src/routes/new.ts
+++ payment/src/routes/new.ts
@@ -1,6 +1,17 @@
 import { requireAuth } from "@m0ticketing/common";
 import express, { Request, Response } from "express";
+import { body } from "express-validator";
 
 const router = express.Router();
 
-router.post("/", requireAuth, (req: Request, res: Response) => {});
+router.post(
+  "/",
+  requireAuth,
+  body("orderId")
+    .not()
+    .isEmpty()
+    .isString()
+    .withMessage("Please provide a valid userId"),
+  body("token").not().isEmpty().withMessage("Please provide a valid token"),
+  (req: Request, res: Response) => {}
+);
