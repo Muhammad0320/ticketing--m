0be1a4bef3ab1c97e23ce509ffee3b59b6161b71
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-14 18:10:58 +0100

    chore: add test that returns a 401 if users tried to pay for a ticket that they dont own

--- payment/src/routes/__test__/new.test.ts
+++ payment/src/routes/__test__/new.test.ts
@@ -1,6 +1,8 @@
 import request from "supertest";
 import { app } from "../../app";
 import mongoose from "mongoose";
+import { Orders } from "../../model/order";
+import { OrderStatus } from "@m0ticketing/common";
 
 it("returns a 404 if there is no order with such id", async () => {
   await request(app)
@@ -13,6 +15,23 @@ it("returns a 404 if there is no order with such id", async () => {
     .expect(404);
 });
 
-it("returns a 401 if users tried to pay for a ticket that they don't own", async () => {});
+it("returns a 401 if users tried to pay for a ticket that they don't own", async () => {
+  const order = await Orders.buildOrder({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    price: 99,
+    version: 0,
+    status: OrderStatus.Created,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  });
+
+  await request(app)
+    .post("/api/payment")
+    .set("Cookie", global.signin())
+    .send({
+      token: "fejvfjvf",
+      orderId: order.id,
+    })
+    .expect(404);
+});
 
 it("returns a 400, if a calcelled order is orderes", async () => {});
