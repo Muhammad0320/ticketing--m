93769e427a766d865b2ce55bcacfebe6b5aed53f
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-14 18:07:49 +0100

    chore: add test that returns a 404 if there is no order with such id

--- payment/src/routes/__test__/new.test.ts
+++ payment/src/routes/__test__/new.test.ts
@@ -1,4 +1,17 @@
-it("returns a 404 if there is no order with such id", async () => {});
+import request from "supertest";
+import { app } from "../../app";
+import mongoose from "mongoose";
+
+it("returns a 404 if there is no order with such id", async () => {
+  await request(app)
+    .post("/api/payment")
+    .set("Cookie", global.signin())
+    .send({
+      token: "fejvfjvf",
+      orderId: new mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
 
 it("returns a 401 if users tried to pay for a ticket that they don't own", async () => {});
 
