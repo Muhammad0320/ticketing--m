e4a5fc26a0bbce8abe864fc14eff10f97d5eaacd
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-14 08:19:38 +0100

    chore: add setup func for order cancelled listenner

--- payment/src/events/listener/__test__/OrdercancelledListener.test.ts
+++ payment/src/events/listener/__test__/OrdercancelledListener.test.ts
@@ -1,3 +1,36 @@
-const setup = () => {};
+import mongoose from "mongoose";
+import { Orders } from "../../../model/order";
+import { natsWrapper } from "../../../natsWrapper";
+import { OrderCancelledListener } from "../OrderCancelledListener";
+import { OrderCancelledEvent, OrderStatus } from "@m0ticketing/common";
+import { Message } from "node-nats-streaming";
+
+const setup = () => {
+  const listener = new OrderCancelledListener(natsWrapper.client);
+
+  const order = await Orders.buildOrder({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    price: 99,
+    version: 0,
+    status: OrderStatus.Created,
+    userId: "shit user",
+  });
+
+  const data: OrderCancelledEvent["data"] = {
+    id: order.id,
+    version: order.version + 1,
+    ticket: {
+      id: new mongoose.Types.ObjectId().toHexString(),
+    },
+  };
+
+  // @ts-ignore
+
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, order, data, msg };
+};
 
 it("cancels the order", async () => {});
