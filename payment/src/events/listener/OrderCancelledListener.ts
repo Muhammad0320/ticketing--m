e45ab2dcf92b44370f37220e6a71d616f565f996
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-14 08:11:29 +0100

    chore: update order cancelled event

--- payment/src/events/listener/OrderCancelledListener.ts
+++ payment/src/events/listener/OrderCancelledListener.ts
@@ -1,1 +1,31 @@
-export class OrderCancelledListener {}
+import {
+  Listener,
+  OrderCancelledEvent,
+  OrderStatus,
+  Subjects,
+} from "@m0ticketing/common";
+import { queueGroupName } from "./queueGroupName";
+import { Message } from "node-nats-streaming";
+import { Orders } from "../../model/order";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const version = data.version - 1;
+
+    const order = await Orders.findOneAndUpdate(
+      { _id: data.id, version },
+      { status: OrderStatus.Cancelled },
+      { new: true }
+    );
+
+    if (!order) {
+      throw new Error("Orther not found");
+    }
+
+    msg.ack();
+  }
+}
