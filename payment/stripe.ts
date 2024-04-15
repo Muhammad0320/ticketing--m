0eb34edc3d65874a174b681ecd70676c4fc42e01
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-15 03:59:04 +0100

    chore: add stripe export

--- payment/stripe.ts
+++ payment/stripe.ts
@@ -1,1 +1,7 @@
-const stripe = "";
+import Stripe from "stripe";
+
+if (!process.env.STRIPE_KEY) {
+  throw new Error("Stripe key not found");
+}
+
+export const stripe = new Stripe(process.env.STRIPE_KEY);
