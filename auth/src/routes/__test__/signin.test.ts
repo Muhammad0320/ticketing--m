3b1226ddc1f306faee1861cc633cc52e4cfca1d8
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-15 16:52:20 +0100

    chore: update signout

--- auth/src/routes/__test__/signin.test.ts
+++ auth/src/routes/__test__/signin.test.ts
@@ -3,31 +3,31 @@ import { app } from "../../app";
 
 it("returns 400 on invalid email", async () => {
   return supertest(app)
-    .post("api/users/signin")
+    .post("/api/users/signin")
     .send({ email: "test@example.com", password: "password" })
     .expect(400);
 });
 
 it("returns 400 in invalid password", async () => {
   await supertest(app)
-    .post("api/users/signup")
+    .post("/api/users/signup")
     .send({ email: "test@test.com", password: "passwords" })
     .expect(201);
 
   await supertest(app)
-    .post("api/users/signin")
+    .post("/api/users/signin")
     .send({ email: "test@test.com", password: "shittttttt" })
     .expect(400);
 });
 
 it("set a cookie when user successfully signups", async () => {
   await supertest(app)
-    .post("api/users/signup")
+    .post("/api/users/signup")
     .send({ email: "test@test.com", password: "passwords" })
     .expect(201);
 
   const response = await supertest(app)
-    .post("api/users/signin")
+    .post("/api/users/signin")
     .send({ email: "test@test.com", password: "passwords" })
     .expect(200);
 
--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -3,16 +3,22 @@ import { app } from "../../app";
 
 it("clears the cookie after signout", async () => {
   await supertest(app)
-    .post("api/users/signup")
+    .post("/api/users/signup")
     .send({ email: "foo@example.com", password: "passwords" })
     .expect(201);
 
   const response = await supertest(app)
-    .post("api/users/signout")
+    .post("/api/users/signout")
     .send({})
     .expect(200);
 
-  expect(response.get("Set-Cookie")[0]).toEqual(
+  // if (!response) {
+  //   return  ;
+
+  //   // throw new Error(' ')
+  // }
+
+  expect(response.get("Set-Cookie")![0]).toEqual(
     "express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
   );
 });
