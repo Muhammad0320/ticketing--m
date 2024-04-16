03dc23fe3ea2b77c0d82e415590f93ee9a3b2bbf
Muhammad0320 <balogunmuhammadawwal@gmail.com>
2024-04-16 17:44:57 +0100

    chore: update signout test

--- auth/src/routes/__test__/signout.test.ts
+++ auth/src/routes/__test__/signout.test.ts
@@ -4,7 +4,7 @@ import { app } from "../../app";
 it("clears the cookie after signout", async () => {
   await supertest(app)
     .post("/api/users/signup")
-    .send({ email: "foo@example.com", password: "passwords" })
+    .send({ email: "test@test.com", password: "passwords" })
     .expect(201);
 
   const response = await supertest(app)
@@ -19,6 +19,6 @@ it("clears the cookie after signout", async () => {
   // }
 
   expect(response.get("Set-Cookie")![0]).toEqual(
-    "express:sess=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
+    "session=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT; httponly"
   );
 });
